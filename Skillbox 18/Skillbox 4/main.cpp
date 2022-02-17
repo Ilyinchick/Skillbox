fe6faaf47b697f01787eb4ac691ce40e8d2730b2
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-17 20:56:09 +0300

    Copmplete exercise 4

---
+++ Skillbox 18/Skillbox 4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void evendigits(long long number, int* ans);
+
+int main() {
+    int ans = 0;
+    long long number = 9223372036854775806;
+    evendigits(number, &ans);
+    std::cout << ans;
+    return 0;
+}
+
+void evendigits(long long number, int* ans) {
+    if (number > 0) {
+        evendigits(number / 10, ans);
+    }
+    if (number % 2 == 0 && number > 0) *ans += 1;
+}
