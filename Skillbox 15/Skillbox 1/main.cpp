08c9c3a5168356360eb07ae8558ec1f03207c51e
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-17 18:29:02 +0300

    Delete comemnt

--- Skillbox 15/Skillbox 1/main.cpp
+++ Skillbox 15/Skillbox 1/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     int a[] = {10,1,-3,4,-1,2,1,-5,4};
     int max = a[0], summ, left, right;
-//asd
+
     for (int i = 0; i < 9; i++) {
         for (int j = 8; j >= 0; j--) {
             summ = a[0];
