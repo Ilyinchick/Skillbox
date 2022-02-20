9ef46e3143377de52b70bc2ba1cb8ae5daeb30f5
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-20 17:32:11 +0300

    fixed 15-1 O(n)

--- Skillbox 15/Skillbox 1/main.cpp
+++ Skillbox 15/Skillbox 1/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
 int main() {
-    int a[] = {-5, 60, 10, -50, -500, 80, 10, -5, 4};
-    int summ = 0, left, right, maxSumm = a[0];
+    int a[] = {-5, 10, 500, -50, -1000, 400, 600, -5, 4};
+    int summ = a[0], left = 0, right = 0, maxSumm = a[0];
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 1; i < 9; i++) {
+
         summ += a[i];
-
-        if (summ < 0) {
-            summ = 0;
-            left = i + 1;
-        }
-
-        if (maxSumm < summ) {
+        if (summ > maxSumm) {
             maxSumm = summ;
             right = i;
         }
+        if (a[i] > summ) {
+            summ = a[i];
+            left = i;
+        }
 
     }
-    std::cout << left << " - " << right;
+    std::cout << left << " - " << right << "    " << maxSumm;
 
     return 0;
 }
