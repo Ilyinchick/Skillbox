6ade6c02c5bb2716919507daeca3cbf55603e642
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-20 18:44:17 +0300

    another fix to 15-1

--- Skillbox 15/Skillbox 1/main.cpp
+++ Skillbox 15/Skillbox 1/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 
 int main() {
-    int a[] = {-5, 10, 500, -50, -1000, 400, 600, -5, 4};
-    int summ = a[0], left = 0, right = 0, maxSumm = a[0];
+    int a[] = {-5, 300, 900, -50, -2000, 800, 800, -5, 4};
+    int summ = a[0], left = 0, right = 0, maxSumm = a[0], bestLeft, bestRight;
 
     for (int i = 1; i < 9; i++) {
 
         summ += a[i];
+
+        if (a[i] > summ) {
+            summ = a[i];
+            if (i > right)  bestLeft = i;
+            else left = i;
+        }
         if (summ > maxSumm) {
             maxSumm = summ;
             right = i;
         }
-        if (a[i] > summ) {
-            summ = a[i];
-            left = i;
+        if (bestLeft < right)   left = bestLeft;
+        if (maxSumm < maxSumm - a[left]) {
+            maxSumm = maxSumm - a[left];
+            left++;
         }
 
     }
