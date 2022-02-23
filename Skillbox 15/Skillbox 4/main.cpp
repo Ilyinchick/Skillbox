7f47c98892eecad4749d28c0455efe0794471442
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-23 13:48:48 +0300

    Fixed 15-4 without sorting array

--- Skillbox 15/Skillbox 4/main.cpp
+++ Skillbox 15/Skillbox 4/main.cpp
@@ -1,52 +1,78 @@
 #include <iostream>
 
-int partition (int arr[], int low, int high);
+void print(int arr[], int size, int min);
 
-void print(int arr[]);
-
-void quickSort(int arr[], int low, int high);
-
-void swap (int arr[], int a, int b);
+int findMinAbs(int arr[], int size);
 
 int main() {
-    int arr[] = {-100,-50, -5, 1, 10, 15};
+    int size = 5;
+    int arr[] = {-100, -50, -5, 1, 10, 15};
 
-    quickSort(arr, 0, 5);
-    print(arr);
+    print(arr, 5, findMinAbs(arr, 5));
 
     return 0;
 }
 
-int partition (int arr[], int low, int high) {
-    int pivotIndex = high;
+void print(int arr[], int size, int min) {
+    int left, right;
+    bool leftF = false, rightF = false;
 
-    for (int j = 0; j < high; j++)
-    {
-        if (std::abs(arr[j]) > std::abs(arr[pivotIndex])) {
-            swap(arr, j, pivotIndex);
+    if (min == 0) {
+        left = min;
+        right = min + 1;
+    } else if (min == size) {
+        right = min;
+        left = min - 1;
+    } else {
+        left = min;
+        right = min + 1;
+    }
+
+    while (!leftF || !rightF) {
+        if (rightF) {
+            if (!leftF && left == 0) {
+                std::cout << arr[left] << " ";
+                leftF = true;
+            } else {
+                std::cout << arr[left] << " ";
+                left--;
+            }
+            continue;
+        } else if (leftF) {
+            if (right == size && !rightF) {
+                std::cout << arr[right] << " ";
+                rightF = true;
+            } else {
+                std::cout << arr[right] << " ";
+                right++;
+            }
+            continue;
+        }
+
+        if (std::abs(arr[left]) < std::abs(arr[right])) {
+            if (left == 0 && !leftF) {
+                std::cout << arr[left] << " ";
+                left = true;
+            } else {
+                std::cout << arr[left] << " ";
+                left--;
+            }
+        } else {
+            if (right == size && !rightF) {
+                std::cout << arr[right] << " ";
+                rightF = true;
+            } else {
+                std::cout << arr[right] << " ";
+                right++;
+            }
         }
     }
-    return pivotIndex;
 }
 
-void quickSort(int arr[], int low, int high) {
-    int pivot = partition(arr, low, high);
-    if (low < high)
-    {
-        quickSort(arr, low, pivot - 1);
-        quickSort(arr, pivot + 1, high);
+int findMinAbs(int arr[], int size) {
+    int min = 0;
+    for (int i = 1; i < size; i++) {
+        if (std::abs(arr[min]) > std::abs(arr[i])) min = i;
     }
-}
-
-void print(int arr[]) {
-    std::cout << "\n";
-    for (int i = 0; i < 6; i++) {
-        std::cout << arr[i] << " ";
-    }
-}
-
-void swap (int arr[], int a, int b) {
-    int i = arr[a];
-    arr[a] = arr[b];
-    arr[b] = i;
+    return min;
 }
