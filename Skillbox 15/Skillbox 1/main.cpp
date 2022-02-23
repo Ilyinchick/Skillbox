409931db2fbf07b98cced90cd5e1610cb51cb12f
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-23 12:56:34 +0300

    Fixed and accepted in leetcode 15-1. Profit

--- Skillbox 15/Skillbox 1/main.cpp
+++ Skillbox 15/Skillbox 1/main.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+#include <vector>
+
+int maxSubArray(std::vector<int> &nums);
 
 int main() {
-    int a[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int summ = a[0], left = 0, right = 0, maxSumm = a[0], bestLeft;
+    std::vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
 
-    for (int i = 1; i < 9; i++) {
+    maxSubArray(nums);
 
-        if (summ > 0 && a[i] < 0 && std::abs(a[i]) > summ) {
-            summ = a[i];
+    return 0;
+}
+
+int maxSubArray(std::vector<int> &nums) {
+
+    int summ = nums[0], left = 0, right = 0, maxSumm = nums[0], bestLeft;
+
+    for (int i = 1; i < nums.size(); i++) {
+
+        if (summ > 0 && nums[i] < 0 && std::abs(nums[i]) > summ) {
+            summ = nums[i];
         }
 
-        summ += a[i];
+        summ += nums[i];
 
-        if (a[i] > summ) {
-            summ = a[i];
-            if (i > right)  bestLeft = i;
+        if (nums[i] > summ) {
+            summ = nums[i];
+            if (i > right) bestLeft = i;
             else left = i;
         }
         if (summ > maxSumm) {
             maxSumm = summ;
             right = i;
         }
-        if (bestLeft < right)   left = bestLeft;
-        if (maxSumm < maxSumm - a[left]) {
-            maxSumm = maxSumm - a[left];
+        if (bestLeft < right) left = bestLeft;
+        if (maxSumm < maxSumm - nums[left]) {
             left++;
         }
 
     }
-    std::cout << left << " - " << right << "    " << maxSumm;
+    std::cout << left << " - " << right << std::endl;
 
-    return 0;
+    return maxSumm;
+
 }
+
+
+
+
+
+
+
