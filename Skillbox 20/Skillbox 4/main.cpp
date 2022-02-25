c19e2e3021b4da02c4959ce265b2c3751dabeaff
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-02-25 19:28:00 +0300

    Completed 20-4
    Tested transactionCalculate algorith with small amount of bills and it works

---
+++ Skillbox 20/Skillbox 4/main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+
+const std::string path = "C:\\Users\\ailyi\\CLionProjects\\Skillbox\\Skillbox 20\\Skillbox 4\\bank.bin";
+
+int bank[6] = {0, 0, 0, 0, 0, 0};
+
+void fillBank();
+
+int enterSumm();
+
+int calculateBankSumm();
+
+void fillBills();
+
+void calculateTransaction(int* summ, int nominal, int index);
+
+int main() {
+    int summ, bankSumm;
+    std::srand(std::time(nullptr));
+
+    fillBank();
+    fillBills();
+    bankSumm = calculateBankSumm();
+    std::cout << bankSumm;
+    summ = enterSumm();
+
+    if (summ < bankSumm && summ % 100 == 0) {
+        calculateTransaction(&summ, 5000, 5);
+        calculateTransaction(&summ, 2000, 4);
+        calculateTransaction(&summ, 1000, 3);
+        calculateTransaction(&summ, 500, 2);
+        calculateTransaction(&summ, 200, 1);
+        calculateTransaction(&summ, 100, 0);
+    } else {
+        std::cout << "Invalid summ to get." << std::endl;
+    }
+
+    return 0;
+}
+
+void fillBank() {
+    std::ofstream stream(path, std::ios::binary);
+    int dummy;
+    for (int i = 0; i < 1000; i++) {
+        dummy = rand() % 6 + 1;
+        switch(dummy) {
+            case 1 : {
+                dummy = 100;
+                stream << dummy << std::endl;
+                break;
+            } case 2 : {
+                dummy = 200;
+                stream << dummy << std::endl;
+                break;
+            } case 3 : {
+                dummy = 500;
+                stream << dummy << std::endl;
+                break;
+            } case 4 : {
+                dummy = 1000;
+                stream << dummy << std::endl;
+                break;
+            } case 5 : {
+                dummy = 2000;
+                stream << dummy << std::endl;
+                break;
+            } case 6 : {
+                dummy = 5000;
+                stream << dummy << std::endl;
+                break;
+            }
+        }
+    }
+    stream.close();
+}
+
+int enterSumm() {
+    int summ;
+    std::cout << "Enter summ you wat to get: " << std::endl;
+    std::cin >> summ;
+    return summ;
+}
+
+int calculateBankSumm() {
+    int bankSumm = 0, dummy;
+    std::ifstream stream;
+
+    stream.open(path, std::ios::binary);
+    while (!stream.eof()) {
+        stream >> dummy;
+        bankSumm += dummy;
+    }
+    stream.close();
+    return bankSumm;
+}
+
+void fillBills() {
+    std::ifstream stream(path, std::ios::binary);
+    int dummy;
+
+    while (!stream.eof()) {
+        stream >> dummy;
+        switch (dummy) {
+            case 100 : {
+                bank[0]++;
+                break;
+            } case 200 : {
+                bank[1]++;
+                break;
+            } case 500 : {
+                bank[2]++;
+                break;
+            } case 1000 : {
+                bank[3]++;
+                break;
+            } case 2000 : {
+                bank[4]++;
+                break;
+            } case 5000 : {
+                bank[5]++;
+                break;
+            }
+        }
+
+    }
+    stream.close();
+}
+
+void calculateTransaction(int* summ, int nominal, int index) {
+    int countBills = *summ / nominal;
+    if (countBills >= bank[index])  countBills = bank[index];
+    bank[index] -= countBills;
+    *summ -= countBills * nominal;
+    if (index == 0) {
+        if (*summ == 0) {
+            std::cout << "You successfully get your money. Everithing is OK." << std::endl;
+        } else {
+            std::cout << "We dont have enough bills. Sorry." << std::endl;
+        }
+    }
+}
