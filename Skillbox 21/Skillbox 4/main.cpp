d71e1d0e0cf822f89c16f69c45b46a094d834529
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-03-07 18:17:15 +0300

    21-4 moving completed

--- Skillbox 21/Skillbox 4/main.cpp
+++ Skillbox 21/Skillbox 4/main.cpp
@@ -31,17 +31,33 @@ int enterInt();
 
 void fillField();
 
-void initializeEnemyPos(position* pos);
+void initializeEnemyPos(position *pos);
 
 position enterPos();
 
 void printField();
+
+void move();
+
+void enemiesMove();
+
+void enemyMove(character *enemy);
+
+bool isValidCoordinate(int x, int y);
+
+std::string enterCommandMove();
 
 int main() {
     std::srand(std::time(nullptr));
     fillField();
     initialize();
     printField();
+
+    while (true) {
+        printField();
+        move();
+        if (player.health == 0) break;
+    }
 
     return 0;
 }
@@ -56,7 +72,7 @@ void initialize() {
 void initializeEnemy(int i) {
     character newEnemy;
     position pos;
-    newEnemy.name = "Enemy #" + std::to_string(i+1);
+    newEnemy.name = "Enemy #" + std::to_string(i + 1);
     newEnemy.health = rand() % 101 + 50;
     newEnemy.armor = rand() % 51;
     newEnemy.damage = rand() % 16 + 15;
@@ -88,7 +104,7 @@ position enterPos() {
     if (pos.x > 40 || pos.y > 40) {
         std::cout << "Invalid values, try again." << std::endl;
         enterPos();
-    } else  return pos;
+    } else return pos;
 }
 
 std::string enterStr() {
@@ -108,14 +124,14 @@ int enterInt() {
     }
 }
 
-void initializeEnemyPos(position* pos) {
+void initializeEnemyPos(position *pos) {
     int x = rand() % 40, y = rand() % 40;
     if (field[x][y] != '.') {
         initializeEnemyPos(pos);
     } else {
         field[x][y] = 'E';
-        pos -> x = x;
-        pos -> y = y;
+        pos->x = x;
+        pos->y = y;
     }
 }
 
@@ -136,4 +152,84 @@ void printField() {
         }
     }
     std::cout << std::endl;
+}
+
+void move() {
+    field[player.pos.x][player.pos.y] = '.';
+    std::string command = enterCommandMove();
+    if (command == "down") {
+        if (isValidCoordinate(player.pos.x + 1, player.pos.y)) {
+            player.pos.x += 1;
+        }
+    } else if (command == "up") {
+        if (isValidCoordinate(player.pos.x - 1, player.pos.y)) {
+            player.pos.x -= 1;
+        }
+    } else if (command == "right") {
+        if (isValidCoordinate(player.pos.x, player.pos.y + 1)) {
+            player.pos.y += 1;
+        }
+    } else if (command == "left") {
+        if (isValidCoordinate(player.pos.x, player.pos.y - 1)) {
+            player.pos.y -= 1;
+        }
+    }
+    field[player.pos.x][player.pos.y] = 'P';
+    enemiesMove();
+}
+
+std::string enterCommandMove() {
+    std::string command;
+    std::cout << "Enter direction to move (right, left, up or down): " << std::endl;
+    std::cin >> command;
+    if (command == "right" || command == "left" || command == "up" || command == "down") {
+        return command;
+    } else {
+        std::cout << "Invalid command. Try again." << std::endl;
+        enterCommandMove();
+    }
+}
+
+void enemiesMove() {
+    for (int i = 0; i < 5; i++) {
+        field[enemy[i].pos.x][enemy[i].pos.y] = '.';
+        enemyMove(&enemy[i]);
+        field[enemy[i].pos.x][enemy[i].pos.y] = 'E';
+    }
+}
+
+void enemyMove(character *enemy) {
+    int direction = rand() % 4;
+    switch (direction) {
+        case 0: {
+            if (isValidCoordinate(enemy->pos.x - 1, enemy->pos.y)) {
+                enemy->pos.x -= 1;
+            }
+            break;
+        }
+        case 1: {
+            if (isValidCoordinate(enemy->pos.x, enemy->pos.y + 1)) {
+                enemy->pos.y += 1;
+            }
+            break;
+        }
+        case 2: {
+            if (isValidCoordinate(enemy->pos.x + 1, enemy->pos.y)) {
+                enemy->pos.x += 1;
+            }
+            break;
+        }
+        case 3: {
+            if (isValidCoordinate(enemy->pos.x, enemy->pos.y - 1)) {
+                enemy->pos.y -= 1;
+            }
+            break;
+        }
+    }
+
+}
+
+bool isValidCoordinate(int x, int y) {
+    if (x < 0 || x > 40 || y < 0 || y > 40) return false;
+    else return true;
 }
