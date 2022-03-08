36f397352cfd6d2bc862c8fa796ff9ea7e5e5ed3
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-03-08 18:54:12 +0300

    Completed 21-4 with save/load

--- Skillbox 21/Skillbox 4/main.cpp
+++ Skillbox 21/Skillbox 4/main.cpp
@@ -17,7 +17,7 @@ struct character {
 
     void printStats() {
         std::cout << name << " stats: \n" << "Health: " << health << "\nArmor: " << armor << "\nDamage: " << damage
-                  << std::endl;
+                  << std::endl << "Position: " << pos.x << " - " << pos.y << std::endl;
     }
 };
 
@@ -51,7 +51,7 @@ void move();
 
 void enemiesMove();
 
-bool isValidCoordinate(position* pos);
+bool isValidCoordinate(position *pos);
 
 std::string enterCommandMove();
 
@@ -71,11 +71,15 @@ void loadGame();
 
 void loadField();
 
-void loadCharacters();
+void loadPlayer();
+
+void loadEnemies();
 
 void saveGame();
 
-void saveCharacters();
+void savePlayer();
+
+void saveEnemies();
 
 void saveField();
 
@@ -84,15 +88,11 @@ int main() {
 
     fillField();
     initialize();
-    printField();
-    saveField();
-
     while (isAlive(&player) && enemiesAlive()) {
         printField();
-        move();
         player.printStats();
+        move();
     }
-
     std::cout << "Game over." << std::endl;
 
     return 0;
@@ -193,21 +193,29 @@ void printField() {
 }
 
 void move() {
-    field[player.pos.x][player.pos.y] = '.';
+
     std::string command = enterCommandMove();
-    if (command == "down") moveTo(&player, 3);
-    else if (command == "up") moveTo(&player, 1);
-    else if (command == "right") moveTo(&player, 2);
-    else if (command == "left") moveTo(&player, 0);
-    field[player.pos.x][player.pos.y] = 'P';
-    enemiesMove();
+    if (command == "save" || command == "load") {
+        if (command == "save") saveGame();
+        else loadGame();
+    } else {
+        field[player.pos.x][player.pos.y] = '.';
+        if (command == "down") moveTo(&player, 3);
+        else if (command == "up") moveTo(&player, 1);
+        else if (command == "right") moveTo(&player, 2);
+        else if (command == "left") moveTo(&player, 0);
+        field[player.pos.x][player.pos.y] = 'P';
+        enemiesMove();
+    }
+
 }
 
 std::string enterCommandMove() {
     std::string command;
-    std::cout << "Enter direction to move (right, left, up or down): " << std::endl;
+    std::cout << "Enter direction to move (right, left, up or down) or save/load game: " << std::endl;
     std::cin >> command;
-    if (command == "right" || command == "left" || command == "up" || command == "down") {
+    if (command == "right" || command == "left" || command == "up" || command == "down" || command == "save" ||
+        command == "load") {
         return command;
     } else {
         std::cout << "Invalid command. Try again." << std::endl;
@@ -226,7 +234,7 @@ void enemiesMove() {
     }
 }
 
-bool isValidCoordinate(position* pos) {
+bool isValidCoordinate(position *pos) {
     if (pos->x < 0 || pos->x > 40 || pos->y < 0 || pos->y > 40) return false;
     else return true;
 }
@@ -273,7 +281,7 @@ void moveTo(character *object, int direction) {
     }
     if (isValidCoordinate(&newPos)) {
         if (field[newPos.x][newPos.y] == '.') object->pos = newPos;
-        else if (field[newPos.x][newPos.y] == 'E' && object->type == "Player" )  {
+        else if (field[newPos.x][newPos.y] == 'E' && object->type == "Player") {
             fight(&player, findByPos(newPos));
         } else if (field[newPos.x][newPos.y] == 'P' && object->type == "Enemy") {
             fight(object, &player);
@@ -298,7 +306,37 @@ bool enemiesAlive() {
 }
 
 void saveGame() {
+    saveField();
+    savePlayer();
+    saveEnemies();
+}
 
+void loadGame() {
+    loadPlayer();
+    loadField();
+    loadEnemies();
+}
+
+void saveEnemies() {
+    for (int i = 0; i < 5; i++) {
+        std::string newPath = path + enemy[i].name + ".txt";
+        std::ofstream stream;
+        stream.open(newPath);
+        stream << enemy[i].name << std::endl << enemy[i].health << std::endl << enemy[i].armor << std::endl
+               << enemy[i].damage << std::endl << enemy[i].pos.x << std::endl << enemy[i].pos.y << std::endl;
+        stream.close();
+    }
+}
+
+void loadEnemies() {
+    for (int i = 0; i < 5; i++) {
+        std::string newPath = path + enemy[i].name + ".txt";
+        std::ifstream stream;
+        stream.open(newPath);
+        std::getline(stream, enemy[i].name);
+        stream >> enemy[i].health  >> enemy[i].armor >> enemy[i].damage >> enemy[i].pos.x >> enemy[i].pos.y;
+        stream.close();
+    }
 }
 
 void saveField() {
@@ -315,5 +353,33 @@ void saveField() {
 }
 
 void loadField() {
-    
+    std::string fieldPath = path + "field.txt";
+    char dummy;
+    std::ifstream stream;
+    stream.open(fieldPath);
+    for (int i = 0; i < 40; i++) {
+        for (int j = 0; j < 40; j++) {
+            stream >> dummy;
+            field[i][j] = dummy;
+        }
+    }
+    stream.close();
+}
+
+void savePlayer() {
+    std::string newPath = path + "player.txt";
+    std::ofstream stream;
+    stream.open(newPath);
+    stream << player.name << std::endl << player.health << std::endl << player.armor << std::endl << player.damage
+           << std::endl << player.pos.x << std::endl << player.pos.y;
+    stream.close();
+}
+
+void loadPlayer() {
+    std::string newPath = path + "player.txt";
+    std::ifstream stream;
+    int i = 0;
+    stream.open(newPath);
+    stream >> player.name >> player.health >> player.armor >> player.damage >> player.pos.x >> player.pos.y;
+    stream.close();
 }
