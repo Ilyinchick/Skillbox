75b6cd330eb9584afbd10b8ac1653d840a11e2c5
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-03-12 15:48:39 +0300

    Complete 22-2

--- Skillbox 22/Skillbox 2/main.cpp
+++ Skillbox 22/Skillbox 2/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <map>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<std::string, std::string> oMap;
+    int counter = 0;
+    std::string str;
+    while (true) {
+        std::cin >> str;
+        if (str == "exit") break;
+        else if (str == "Next") {
+            std::cout << oMap.begin()->second << std::endl;
+            oMap.erase(oMap.begin()->first);
+        } else  {
+            oMap.insert(std::make_pair(str[0] + std::to_string(counter), str));
+            counter++;
+        }
+    }
     return 0;
 }
