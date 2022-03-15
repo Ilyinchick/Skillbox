3dec2e2e8e3154aa48dd88164c54479242ff2965
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-03-15 17:54:22 +0300

    Completed 24-1

---
+++ Skillbox 24/Skillbox 1/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+struct task {
+    std::string name;
+    int time;
+};
+
+std::vector<task> taskVector;
+
+void printTasks(std::vector<task>* vec);
+
+int main() {
+    std::string input, taskName;
+    bool tsk = false;
+    int time;
+
+    while (true) {
+        std::cin >> input;
+        if (input == "exit") break;
+        else {
+            if (input == "begin") {
+                if (tsk) {
+                    task task;
+                    time = std::time(nullptr) - time;
+                    task.name = taskName;
+                    task.time = time;
+                    taskVector.push_back(task);
+                }
+                tsk = true;
+                std::cout << "Enter task name:" << std::endl;
+                std::cin >> taskName;
+                time = std::time(nullptr);
+            } else if (input == "end") {
+                if (tsk) {
+                    tsk = false;
+                    time = std::time(nullptr) - time;
+                    task task;
+                    task.name = taskName;
+                    task.time = time;
+                    taskVector.push_back(task);
+                } else {
+                    std::cout << "There is no task right now." << std::endl;
+                }
+            } else if (input == "status") {
+                printTasks(&taskVector);
+            } else {
+                std::cout << "Invalid input." << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void printTasks(std::vector<task>* vec) {
+    for (task t: *vec) {
+        std::cout << t.name << ": " << t.time << std::endl;
+    }
+}
