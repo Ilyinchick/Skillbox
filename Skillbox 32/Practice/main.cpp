68a90f015bcb72369a5d9f896fe197072cbb3c7a
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-07-16 20:57:27 +0300

    Proto2 lib

--- Skillbox 32/Practice/main.cpp
+++ Skillbox 32/Practice/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+struct Some {
+    std::string name;
+    std::string surname;
+    int age;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Some s, s2;
+    std::ofstream file("record.json");
+
+
+    s = {"Mike", "Vazovskiy", 20};
+
+    nlohmann::json obj = {
+            {"name", s.name},
+            {"surname", s.surname},
+            {"age", s.age}}, obj2;
+
+    file << obj;
+    file.close();
+
+    std::ifstream input("record.json");
+    input >> obj2;
+
+    s2.name = obj2["name"];
+    s2.surname = obj2["surname"];
+    s2.age = obj2["age"];
+
+    std::cout << s2.name << " " << s2.surname << " " << s2.age << std::endl;
+
+    input.close();
+
+
     return 0;
 }
 
