c65853873ccac4fa7a6c5039cb5a1d7497605375
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-07-21 14:47:23 +0300

    Added DEBUG_MSG and rewrite destructors

--- Skillbox 31/Skillbox_1/main.cpp
+++ Skillbox 31/Skillbox_1/main.cpp
@@ -1,32 +1,62 @@
 #include <iostream>
 
+#ifdef DEBUG
+#define DEBUG_MSG(str) { std::cout << str << std::endl; }
+#else
+#define DEBUG_MSG(str) { std::cout << str << std::endl; }
+#endif
+
+
 class Toy {
 private:
     std::string name;
 
 public:
-    Toy(std::string _name) : name(_name) {};
+    Toy(std::string _name) : name(_name) {
+        DEBUG_MSG("New Toy " << name);
+    };
 
-    Toy() : name("Emty_Name") {};
+    Toy() : name("Emty_Name") {
+        DEBUG_MSG("New empty Toy");
+    };
+
+    ~Toy() {
+        DEBUG_MSG("Deleted Toy " + name)
+    }
+
+
 
     std::string getName() {
         return name;
     }
 
-    Toy& operator=(const Toy& oth) {
+    Toy &operator=(const Toy &oth) {
         if (this == &oth) return *this;
         name = oth.name;
         return *this;
     }
+
 };
 
 class Count {
+
 private:
     int count;
-public:
-    Count(int i) : count(i) {};
 
-    Count() : count(0) {};
+public:
+    Count(int i) : count(i) {
+        DEBUG_MSG("New count " + std::to_string(i));
+    };
+
+    Count() : count(0) {
+        DEBUG_MSG("New empty count");
+    };
+
+    ~Count() {
+        DEBUG_MSG("Deleted Count " + std::to_string(count));
+    }
+
+
 
     void inc() {
         count += 1;
@@ -61,7 +91,7 @@ public:
         refs = new Count(1);
     }
 
-    Shared_ptr_toy(Toy* toy) {
+    Shared_ptr_toy(Toy *toy) {
         ptr_toy = toy;
         refs = new Count(1);
     }
@@ -70,6 +100,15 @@ public:
         ptr_toy = oth.ptr_toy;
         oth.refs->inc();
         refs = oth.refs;
+    }
+
+    ~Shared_ptr_toy() {
+        DEBUG_MSG("Delete Shared_ptr " <<  ptr_toy->getName() << " - " << std::to_string(refs->get()));
+        if (refs->get() <= 1) {
+            delete ptr_toy;
+            delete refs;
+        } else refs->dec();
+
     }
 
     static Shared_ptr_toy *makeShared(std::string name) {
@@ -81,15 +120,11 @@ public:
         return new Shared_ptr_toy(oth);
     }
 
-//    void deletePtr() {
-//        delete ptr_toy;
-//    }
-
     Shared_ptr_toy &operator=(const Shared_ptr_toy &oth) {
         if (this == &oth) return *this;
         if (ptr_toy != nullptr) {
+            if (refs->get() <= 1) delete ptr_toy;
             refs->dec();
-            delete ptr_toy;
         }
         oth.refs->inc();
         ptr_toy = oth.ptr_toy;
@@ -97,16 +132,6 @@ public:
         return *this;
     }
 
-    ~Shared_ptr_toy() {
-        refs->dec();
-        if (refs->get() <= 0) {
-            std::cout << "Delete " << ptr_toy->getName() << std::endl;
-            ptr_toy = nullptr;
-            delete ptr_toy;
-        }
-        refs = nullptr;
-        delete refs;
-    }
 };
 
 
@@ -118,39 +143,37 @@ private:
 
 public:
 
-    Dog(std::string _name, int _age, Shared_ptr_toy* lovely) : name(_name), age(_age), lovely_toy(*lovely) {}
+    Dog(std::string _name, int _age, Shared_ptr_toy *lovely) : name(_name), age(_age), lovely_toy(*lovely) {}
 
-    void setLovely(Shared_ptr_toy* lovely) {
+    void setLovely(Shared_ptr_toy *lovely) {
         lovely_toy = *lovely;
     }
-
 
 };
 
 
 int main() {
 
-    Toy* tBall = new Toy("Ball");
-    Toy* tBone = new Toy("Bone");
+    Toy *tBall = new Toy("Ball");
+    Toy *tBone = new Toy("Bone");
 
 
-    Shared_ptr_toy* ball = new Shared_ptr_toy(tBall);
-    Shared_ptr_toy* bone = new Shared_ptr_toy(tBone);
+    Shared_ptr_toy *ball = new Shared_ptr_toy(tBall);
+    Shared_ptr_toy *bone = new Shared_ptr_toy(tBone);
 
-    Dog* d1 = new Dog("D1", 14, ball);
-    Dog* d2 = new Dog("D2", 12, ball);
-    Dog* d3 = new Dog("D3", 14, bone);
-    Dog* d4 = new Dog("D4", 12, ball);
-    Dog* d5 = new Dog("D5", 14, bone);
-    Dog* d6 = new Dog("D6", 12, ball);
+    Dog *d1 = new Dog("D1", 14, ball);
+    Dog *d2 = new Dog("D2", 12, ball);
+    Dog *d3 = new Dog("D3", 14, bone);
+    Dog *d4 = new Dog("D4", 12, ball);
+    Dog *d5 = new Dog("D5", 14, bone);
+    Dog *d6 = new Dog("D6", 12, ball);
 
     d3->setLovely(ball);
     d5->setLovely(ball);
 
-
     delete bone;
 
-    std::cout << "Hello" << std::endl;
+    delete d1, delete d2, delete d3, delete d4, delete d5, delete d6, delete ball;
 
     return 0;
 }
