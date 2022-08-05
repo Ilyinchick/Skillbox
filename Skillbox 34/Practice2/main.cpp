3e092d0f7f75b08b23f96addb92ec0004e5415bc
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-08-05 13:18:09 +0300

    Practice

--- Skillbox 34/Practice2/main.cpp
+++ Skillbox 34/Practice2/main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QMediaPlayer>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QWidget window;
-    auto* player = new QMediaPlayer(&window);
-    window.resize(200, 100);
-    window.show();
-    return QApplication::exec();
+    if (std::stoi("23") > 20) std::cout << "Bigger";
+//    QApplication a(argc, argv);
+//    QWidget window;
+//    auto* player = new QMediaPlayer(&window);
+//    window.resize(200, 100);
+//    window.show();
+//    return QApplication::exec();
+return 0;
 }
