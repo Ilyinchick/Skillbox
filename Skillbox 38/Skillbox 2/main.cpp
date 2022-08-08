53b7251623f0773253f6bd2a2335c96f8889415b
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-08-08 16:24:07 +0300

    Complete 38-2 with msvc compiler

--- Skillbox 38/Skillbox 2/main.cpp
+++ Skillbox 38/Skillbox 2/main.cpp
@@ -1,14 +1,37 @@
 #include <QApplication>
+#include <QWidget>
 #include <QPushButton>
-#include <QWebView>
+#include <QHBoxLayout>
+#include <QPlainTextEdit>
 
-int main(int argc, char *argv[]) {
+#include <QWebEngineView>
+
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    QWidget w;
+    QHBoxLayout layout(&w);
+    QWebEngineView view;
+    QPlainTextEdit text;
+
+    layout.addWidget(&text);
+    layout.addWidget(&view);
+
+    view.setMinimumSize(100, 100);
+    text.setMinimumSize(100, 100);
+
+    layout.setContentsMargins(10, 10, 10, 10);
+
+    text.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    view.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    QObject::connect(&text, &QPlainTextEdit::textChanged, [&text, &view] () {
+        view.setHtml(text.toPlainText());
+        view.update();
+    });
+
+    view.setHtml("<h1> Start typing </h1>");
+
+    w.show();
+    return a.exec();
 }
-
-
-#include "main.moc"
