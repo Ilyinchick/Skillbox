35e02a9fd71f49422382b5ca64ae3538da16323c
Ilyinchick <alexey.ilyinchick@gmail.com>
2022-08-20 16:11:35 +0300

    update ConverterJSON class and resources
    added following functions:
    GetTextDocuments
    GetResponceLimit
    GetRequests
    etc. getDoc, getJson, getCongifJson, getRequestsJson
    
    Also added paths to json files

--- search_engine/include/ConverterJSON.h
+++ search_engine/include/ConverterJSON.h
@@ -1,10 +1,56 @@
 #include <string>
+#include <vector>
+#include "nlohmann/json.hpp"
 
 class ConverterJSON {
+
 public:
-    std::string getDoc(const std::string& path);
 
-    void testConfigJson(const std::string& path);
+    const std::string CONFIG_PATH = R"(C:\Users\ailyi\CLionProjects\Skillbox\search_engine\manage\config.json)";
+    const std::string REQUESTS_PATH = R"(C:\Users\ailyi\CLionProjects\Skillbox\search_engine\manage\requests.json)";
+    const std::string ANSWERS_PATH = R"(C:\Users\ailyi\CLionProjects\Skillbox\search_engine\manage\answers.json)";
 
-    void testRequestsJson(const std::string& path);
+    ConverterJSON() = default;
+
+/**
+* Метод получения содержимого файлов
+* @return Возвращает список с содержимым файлов перечисленных
+* в config.json
+*/
+    std::vector<std::string> GetTextDocuments();
+
+/**
+* Метод считывает поле max_responses для определения предельного
+* количества ответов на один запрос
+* @return
+*/
+    int GetResponsesLimit();
+
+/**
+* Метод получения запросов из файла requests.json
+* @return возвращает список запросов из файла requests.json
+*/
+    std::vector<std::string> GetRequests();
+
+/**
+* Положить в файл answers.json результаты поисковых запросов
+*/
+    void putAnswers(std::vector<std::vector<std::pair<int, float>>> answers);
+
+    void createAnswers();
+
+private:
+    static std::string getDoc(const std::string &path);
+
+    static void testConfigJson(const std::string &path);
+
+    static void testRequestsJson(const std::string &path);
+
+    static nlohmann::json getJson(const std::string& path);
+
+    nlohmann::json getConfigJson();
+
+    nlohmann::json getRequestsJson();
+
+
 };
--- search_engine/src/ConverterJSON.cpp
+++ search_engine/src/ConverterJSON.cpp
@@ -2,30 +2,32 @@
 #include "fstream"
 #include "iostream"
 #include "fs_exception.h"
-#include "nlohmann/json.hpp"
+
 
 const std::string VERSION = "0.1";
 
-
-std::string ConverterJSON::getDoc(const std::string& path) {
+// opens file and returns its data with string
+// throws FileNotFoundException and EmptyFileException if path is invalid or file is empty
+std::string ConverterJSON::getDoc(const std::string &path) {
     std::ifstream file;
     std::string answer, buff;
 
     file.open(path);
-    if (!file.is_open())  throw FileNotFoundException();
+    if (!file.is_open()) throw FileNotFoundException();
 
-    while(file) {
+    while (file) {
         std::getline(file, buff);
         answer.append(buff.append(" "));
         buff.clear();
     }
 
-    if (answer.length() <= 1)   throw EmptyFileException();
+    if (answer.length() <= 1) throw EmptyFileException();
 
     return answer;
 }
 
-void ConverterJSON::testConfigJson(const std::string& path) {
+//test
+void ConverterJSON::testConfigJson(const std::string &path) {
     std::ifstream file(path);
     if (!file.is_open()) throw FileNotFoundException();
 
@@ -38,14 +40,125 @@ void ConverterJSON::testConfigJson(const std::string& path) {
 
 }
 
+//test
 void ConverterJSON::testRequestsJson(const std::string &path) {
-    std::ifstream file(path);
     nlohmann::json dict;
+    try {
+        dict = getJson(path);
+        if (!dict.contains("requests")) throw NoRequestsFieldException();
+        if (dict.find("requests").value().empty()) throw EmptyRequestsFieldException();
+    }
+    catch (FileNotFoundException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (EmptyFileException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+}
+
+
+// Returns vector with data in files which specified in config.json in field "files"
+std::vector<std::string> ConverterJSON::GetTextDocuments() {
+    std::vector<std::string> list;
+    nlohmann::json doc = getConfigJson();
+    for (std::string s: doc.find("files").value()) {
+        try {
+            list.push_back(getDoc(s));
+        }
+        catch (FileNotFoundException &ex) {
+            std::cout << ex.what() << " in ConverterJSON.cpp 62" << std::endl;
+        }
+        catch (EmptyFileException &ex) {
+            std::cout << ex.what() << " in ConverterJSON.cpp 62" << std::endl;
+        }
+    }
+    return list;
+}
+
+//returns int value of "max_responses" fild in config.json
+int ConverterJSON::GetResponsesLimit() {
+    return getConfigJson().find("config")->find("max_responses").value();
+}
+
+//returns list of requests in requests.json
+std::vector<std::string> ConverterJSON::GetRequests() {
+    std::vector<std::string> requests;
+    nlohmann::json doc = getRequestsJson();
+    for (std::string s: doc.find("requests").value()) {
+        requests.push_back(s);
+    }
+    return requests;
+}
+
+void ConverterJSON::putAnswers(std::vector<std::vector<std::pair<int, float>>> answers) {
+
+}
+
+// throws FileNotFoundException and EmptyFileException if path or dile is invalid, otherwise return json
+nlohmann::json ConverterJSON::getJson(const std::string &path) {
+    std::ifstream file(path);
+    nlohmann::json doc;
 
     if (!file.is_open()) throw FileNotFoundException();
-    file >> dict;
-    if (dict.empty()) throw EmptyFileException();
-    if (!dict.contains("requests")) throw NoRequestsFieldException();
-    if (dict.find("requests").value().empty()) throw EmptyRequestsFieldException();
+    file >> doc;
+    if (doc.empty()) throw EmptyFileException();
 
+    return doc;
+}
+
+// basic checks. Returns json with data if file and path are valid, otherwise returns empty file
+nlohmann::json ConverterJSON::getConfigJson() {
+    nlohmann::json doc;
+    try {
+        doc = getJson(CONFIG_PATH);
+        if (!doc.contains("config")) throw NoConfigFieldException();
+        if (doc.find("config").value().empty()) throw ConfigFieldIsEmptyException();
+        if (doc.find("config")->find("version").value() != VERSION) throw IncorrectVersionJsonException();
+    }
+    catch (FileNotFoundException &ex) {
+        std::cout << ex.what();
+    }
+    catch (EmptyFileException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (NoConfigFieldException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (ConfigFieldIsEmptyException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (IncorrectVersionJsonException &ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    return doc;
+}
+
+//returns requests.json if its data is valid
+nlohmann::json ConverterJSON::getRequestsJson() {
+    nlohmann::json doc;
+
+    try {
+        doc = getJson(REQUESTS_PATH);
+    }
+    catch (FileNotFoundException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (EmptyFileException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (NoRequestsFieldException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (EmptyRequestsFieldException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return doc;
+}
+
+void ConverterJSON::createAnswers() {
+    std::ofstream stream(ANSWERS_PATH);
+    nlohmann::json doc = {"asd: {asd}"};
+    stream << doc;
 }
